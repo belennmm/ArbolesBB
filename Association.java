0f37e393c46706cdeb8941ef77afa1ea837e32e4
belennmm <belenmmonterroso@gmail.com>
2024-03-19 22:52:50 -0600

    asociación

---
+++ Association.java
@@ -0,0 +1,20 @@
+/**
+ * Clase que representa la asociación.
+ */
+class Association<K, V> {
+    private K key;
+    private V value;
+
+    public Association(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+}
